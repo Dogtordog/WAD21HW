39713d69212e9368dec364a02747ed0046d4cbbe
Dogtordog <panimarkerik@gmail.com>
2021-12-11 15:44:16 +0200

    ran code through beautifier

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
- user: "postgres",
- password: "dba",
- database: "waddata",
- host: "localhost",
- port: "5432"
+	user: "postgres",
+	password: "dba",
+	database: "waddata",
+	host: "localhost",
+	port: "5432"
 });
 module.exports = pool;
